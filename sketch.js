a591ca1474c580b66519cbdfd766bd875b5c9f94
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-02 13:35:44 -0500

    Multiple random crashing balls

--- sketch.js
+++ sketch.js
@@ -1,20 +1,48 @@
-let x = 450;
-let y = [180, 20, 400, 300];
-let speed = 5;
+let balls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(200, 250, 250);
+
+  for (var i = 0; i < random(30, 120); i++) {
+    balls.push(new Ball());
+  }
 }
 
 function draw() {
-  for (var i = 0; i < y.length; i++) {
-    ellipse(x, y[i], 50, 50);
-    noStroke();
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].show();
+  }
+}
+
+class Ball {
+  constructor() {
+    this.c = random(10, 40);
+    this.r = Math.sqrt(this.c);
+    this.x = random(0, width - this.r);
+    this.y = random(0, height - this.r);
+    this.xSpeed = 3;
+    this.ySpeed = 3;
+
+    this.colors = {
+      r: random(random(48, 200), 255),
+      g: random(random(99, 255), random(50, 255)),
+      b: random(210, random(0, 170))
+    };
   }
 
-  x = x + speed;
-  if (x >= windowWidth - 25 || x < 25) {
-    speed = -speed;
+  show() {
+    ellipse(this.x, this.y, this.c, this.c);
+    noStroke();
+    fill(this.colors.r, this.colors.g, this.colors.b);
+
+    if (this.x >= (width - this.r) || this.x <= this.r)
+      this.xSpeed = -1 * this.xSpeed + 1;
+
+    if (this.y >= (height - this.r) || this.y <= this.r)
+      this.ySpeed = -1 * this.ySpeed + 1;
+
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
   }
 }
